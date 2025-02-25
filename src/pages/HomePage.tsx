9b934c10eaa6dcae760e448aaffa44d8632f62d9
Kind606 <kevin.hellgren@hotmail.com>
2025-02-25 11:13:39 +0100

    Added toggle

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { mockedPhoto } from "../data";
 
 export default function HomePage() {
+  const [likedPhotos, setLikedPhotos] = useState<string[]>([]);
+
+  const toogleLikedPhoto = (id: string) => {
+    if (likedPhotos.includes(id)) {
+      setLikedPhotos(likedPhotos.filter((photoid) => photoid !== id));
+    } else {
+        setLikedPhotos([...likedPhotos, id]);
+    }
+
+  };
   return (
     <main className="grid gap-4">
       {mockedPhoto.map((photo) => (
@@ -8,7 +19,9 @@ export default function HomePage() {
           <img src={photo.url} alt="todo..." className="w-full" />
           <section className="absolute flex justify-between bottom-0 w-full p-2 bg-green-900/60 backdrop-blur-sm text-green-50">
             <p>{photo.author}</p>
-            <button>🤍</button>
+            <button onClick={() => toogleLikedPhoto(photo.id)}>
+              {likedPhotos.includes(photo.id) ? "❤️" : "🤍"}
+            </button>
           </section>
         </article>
       ))}
